906be42ed9572c0610f3980751e0b1dff88ed010
nijat <nijat.de@gmail.com>
2022-08-08 07:15:26 +0200

    Multiple Beans same Type

--- Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/ConfigApp.java
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/ConfigApp.java
@@ -13,9 +13,17 @@ public class ConfigApp {
         return new FullTimeMentor();
     }
 
-
+//    @Bean(name="p1")
     @Bean
+    @Primary
     PartTimeMentor partTimeMentor(){
+
+        return new PartTimeMentor();
+    }
+
+//    @Bean(name="p2")
+    @Bean
+    PartTimeMentor partTimeMentor2(){
 
         return new PartTimeMentor();
     }
