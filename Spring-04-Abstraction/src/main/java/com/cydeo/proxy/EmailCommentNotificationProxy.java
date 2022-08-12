6613c3debfac984b67d3aab80fb464fadecbfa1e
nijat <nijat.de@gmail.com>
2022-08-12 08:50:22 +0200

    Abstraction

--- Spring-04-Abstraction/src/main/java/com/cydeo/proxy/EmailCommentNotificationProxy.java
+++ Spring-04-Abstraction/src/main/java/com/cydeo/proxy/EmailCommentNotificationProxy.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
-
+@Component
 public class EmailCommentNotificationProxy implements CommentNotificationProxy{
     @Override
     public void sendComment(Comment comment) {
