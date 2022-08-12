6ee96d2d890d0d8ad9c1eaf2c538d226b76aaa0a
nijat <nijat.de@gmail.com>
2022-08-12 10:11:07 +0200

    BeanScope

--- Spring-05-Core-BeanScope/src/main/java/com/cydeo/service/CommentService.java
+++ Spring-05-Core-BeanScope/src/main/java/com/cydeo/service/CommentService.java
@@ -10,8 +10,9 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
+//@Scope("prototype")
 //@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+@Lazy
 public class CommentService {
 
     private final CommentRepository commentRepository;
@@ -20,6 +21,7 @@ public class CommentService {
     public CommentService(CommentRepository commentRepository, @Qualifier("PUSH") CommentNotificationProxy commentNotificationProxy) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
+        System.out.println("Hello");
     }
 
     public void publishComment(Comment comment){
