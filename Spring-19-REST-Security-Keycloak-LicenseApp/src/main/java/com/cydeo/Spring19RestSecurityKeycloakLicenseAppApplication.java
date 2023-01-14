18f30d2151b17a9e159e6328745d80b64feef40f
nijat <nijat.de@gmail.com>
2023-01-14 09:06:04 +0100

    Spring Rest Security Keycloak LicenseApp controller and service

--- Spring-19-REST-Security-Keycloak-LicenseApp/src/main/java/com/cydeo/Spring19RestSecurityKeycloakLicenseAppApplication.java
+++ Spring-19-REST-Security-Keycloak-LicenseApp/src/main/java/com/cydeo/Spring19RestSecurityKeycloakLicenseAppApplication.java
@@ -2,12 +2,20 @@ package com.cydeo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class Spring19RestSecurityKeycloakLicenseAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Spring19RestSecurityKeycloakLicenseAppApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
 	}
 
 }
--- Spring-19-REST-Security-Keycloak-OrganizationsApp/src/main/java/com/cydeo/config/SecurityConfig.java
+++ Spring-19-REST-Security-Keycloak-OrganizationsApp/src/main/java/com/cydeo/config/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -19,12 +18,11 @@ import org.springframework.security.web.authentication.session.SessionAuthentica
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(jsr250Enabled = true)
 public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
-        http.authorizeRequests()
+        http.authorizeHttpRequests()
                 .anyRequest()
                 .permitAll();
         http.csrf().disable();
