15b9bf5b735fea31fe0a6c3c8fdb77655ccc7ae4
nijat <nijat.de@gmail.com>
2023-01-16 19:55:22 +0100

    Spring Rest Security Keycloak LicenseApp update

--- Spring-19-REST-Security-Keycloak-LicenseApp/src/main/java/com/cydeo/Spring19RestSecurityKeycloakLicenseAppApplication.java
+++ Spring-19-REST-Security-Keycloak-LicenseApp/src/main/java/com/cydeo/Spring19RestSecurityKeycloakLicenseAppApplication.java
@@ -2,11 +2,12 @@ package com.cydeo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
+@EnableFeignClients
 public class Spring19RestSecurityKeycloakLicenseAppApplication {
 
 	public static void main(String[] args) {
