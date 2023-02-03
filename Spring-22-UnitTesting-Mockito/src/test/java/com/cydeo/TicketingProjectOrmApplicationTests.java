dc7844e5ee3d8ad392dd545e8a373b14e3fdb58f
nijat <nijat.de@gmail.com>
2023-02-03 19:57:48 +0100

    Spring UnitTesting Mockito repository and enums

--- Spring-22-UnitTesting-Mockito/src/test/java/com/cydeo/TicketingProjectOrmApplicationTests.java
+++ Spring-22-UnitTesting-Mockito/src/test/java/com/cydeo/TicketingProjectOrmApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Spring22UnitTestingMockitoApplicationTests {
+class TicketingProjectOrmApplicationTests {
 
 	@Test
 	void contextLoads() {
